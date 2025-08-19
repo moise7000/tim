72ddafcb1f9d6e57998f5dea38fbaec81a78e83b
moise7000 <77933570+moise7000@users.noreply.github.com>
2025-08-19 16:01:36 +0200

    [-]

---
+++ web/pages/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "index.vue"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
